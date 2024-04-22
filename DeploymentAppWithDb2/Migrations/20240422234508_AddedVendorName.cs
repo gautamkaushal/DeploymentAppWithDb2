eda42bda09d95fc4437d9750c5dda5ec399423b3
Gautam Kaushal <gautamkaushal@gautamkaushal.onmicrosoft.com>
2024-04-22 19:52:32 -0400

    Added Vendor Name

---
+++ DeploymentAppWithDb2/Migrations/20240422234508_AddedVendorName.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeploymentAppWithDb2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedVendorName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "VendorName",
+                table: "Expenses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VendorName",
+                table: "Expenses");
+        }
+    }
+}
